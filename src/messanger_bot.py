44939a6b8b46d7119ab57a7ef3688a25e6c1fcce
bgozlinski <bartlomiej.gozlinski@gmail.com>
2023-06-08 01:50:35 +0200

    requirements.txt update

--- src/messanger_bot.py
+++
@@ -1,8 +0,0 @@
-from fbchat import Client
-from fbchat.models import Message
-
-# facebook user credentials
-username = "username.or.email"
-password = "password"
-# login
-client = Client(username, password)
